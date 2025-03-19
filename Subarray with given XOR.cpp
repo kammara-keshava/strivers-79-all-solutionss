67678e723a89ce04cb563533e7b0f9369f3a0ee9
kammara-keshava <151363158+kammara-keshava@users.noreply.github.com>
2025-03-19 22:48:33 +0530

    Create Subarray with given XOR.cpp

---
+++ Subarray with given XOR.cpp
@@ -0,0 +1,14 @@
+int solve(int* A, int n1, int B) {
+    int xr=0;
+    
+    map<int,int> mpp;
+    mpp[xr]++;
+    int c=0;
+    for(int i=0;i<n1,i++){
+        xr=xr^a[i];
+        int x=xr^b;
+        int mpp[x];
+        mpp[xr]++;
+    }
+    return c;
+}
